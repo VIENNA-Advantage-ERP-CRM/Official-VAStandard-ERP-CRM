4448c24ade3f289eb45b0d367154dcc926642eff
Meghraj Salunke <meghrajsalunke.vienna@gmail.com>
2023-01-03 12:45:37 +0530

    ExcelExport issue resolved

--- VASLogic/Models/PostingModel.cs
+++ VASLogic/Models/PostingModel.cs
@@ -77,75 +77,22 @@ namespace VIS.Models
                 {
                     sql.Clear();
                     //special check for get Element List once based on AccountSchemaId
-                    if (j == 1)
-                        elementList = GetElementType(Util.GetValueOfInt(dsFact.Tables[0].Rows[j-1]["C_ACCTSCHEMA_ID"]));
-                    sql.Append(GetDataName(elementList, Ad_Table_Id, Record_Id, Util.GetValueOfInt(dsFact.Tables[0].Rows[j-1]["C_ACCTSCHEMA_ID"])));
+                    elementList = GetElementType(Util.GetValueOfInt(dsFact.Tables[0].Rows[j - 1]["C_ACCTSCHEMA_ID"]));
+                    sql.Append(GetDataName(elementList, Ad_Table_Id, Record_Id, Util.GetValueOfInt(dsFact.Tables[0].Rows[j - 1]["C_ACCTSCHEMA_ID"])));
                     DataSet ds = DB.ExecuteDataset(sql.ToString());
-                    DataTable dt = new DataTable();
                     //Setting Table Name  
-                    dt.TableName = Util.GetValueOfString(dsFact.Tables[0].Rows[j-1]["Name"]);
                     if (ds != null && ds.Tables[0].Rows.Count > 0)
                     {
-                        //Add Columns  
-                        dt.Columns.Add("ad_org_id", typeof(string));
-                        dt.Columns.Add("account_id", typeof(string));
-                        dt.Columns.Add("AMTACCTDR", typeof(string));
-                        dt.Columns.Add("AMTACCTCR", typeof(string));
-                        dt.Columns.Add("m_product_id", typeof(string));
-                        dt.Columns.Add("C_BPARTNER_ID", typeof(string));
-                        dt.Columns.Add("userelement8_id", typeof(string));
-                        dt.Columns.Add("userelement1_id", typeof(string));
-                        dt.Columns.Add("userelement4_id", typeof(string));
-                        dt.Columns.Add("userelement2_id", typeof(string));
-                        dt.Columns.Add("c_activity_id", typeof(string));
-                        dt.Columns.Add("ad_orgtrx_id", typeof(string));
-                        dt.Columns.Add("c_project_id", typeof(string));
-                        dt.Columns.Add("c_campaign_id", typeof(string));
-                        dt.Columns.Add("userelement9_id", typeof(string));
-                        dt.Columns.Add("userelement3_id", typeof(string));
-                        dt.Columns.Add("userelement5_id", typeof(string));
-                        dt.Columns.Add("userelement6_id", typeof(string));
-                        dt.Columns.Add("userelement7_id", typeof(string));
-
-                        //add data
-                        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-                        {
-                            dt.Rows.Add(new Object[]{
-                             ds.Tables[0].Rows[i]["AD_ORG_ID"],
-                             ds.Tables[0].Rows[i]["account_id"],
-                             ds.Tables[0].Rows[i]["ACCDEBIT"],
-                             ds.Tables[0].Rows[i]["ACCCREDIT"],
-                             ds.Tables[0].Rows[i]["m_product_id"],
-                             ds.Tables[0].Rows[i]["C_BPARTNER_ID"],
-                             ds.Tables[0].Rows[i]["userelement8_id"],
-                             ds.Tables[0].Rows[i]["userelement1_id"],
-                             ds.Tables[0].Rows[i]["userelement4_id"],
-                             ds.Tables[0].Rows[i]["userelement2_id"],
-                             ds.Tables[0].Rows[i]["c_activity_id"],
-                             ds.Tables[0].Rows[i]["ad_orgtrx_id"],
-                             ds.Tables[0].Rows[i]["c_project_id"],
-                             ds.Tables[0].Rows[i]["c_campaign_id"],
-                             ds.Tables[0].Rows[i]["userelement9_id"],
-                             ds.Tables[0].Rows[i]["userelement3_id"],
-                             ds.Tables[0].Rows[i]["userelement5_id"],
-                             ds.Tables[0].Rows[i]["userelement6_id"],
-                             ds.Tables[0].Rows[i]["userelement7_id"]
-                             });
-                        }
-                        dt.AcceptChanges();
-                        #endregion
-
-                        #region CreateExcel
                         // Set AccountSchema Name as Sheet Name 
                         Regex pattern = new Regex("[/*?:\r]|[-]");//Added Special Characters which will be Replaced from string if exists
-                        var ws = wb.Worksheets.Add(pattern.Replace(Util.GetValueOfString(dsFact.Tables[0].Rows[j - 1]["Name"]),"-"));
+                        var ws = wb.Worksheets.Add(pattern.Replace(Util.GetValueOfString(dsFact.Tables[0].Rows[j - 1]["Name"]), "-"));
                         //Set Excel file header
-                        ws.Cell("A1").Value = Util.GetValueOfString(dsFact.Tables[0].Rows[j-1]["Name"]);
+                        ws.Cell("A1").Value = Util.GetValueOfString(dsFact.Tables[0].Rows[j - 1]["Name"]);
                         var range = ws.Range("A1:L1");
                         range.Merge().Style.Font.SetBold().Font.FontSize = 12;
                         ws.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
 
-                        if (dt.Rows.Count > 0)
+                        if (ds.Tables[0].Rows.Count > 0)
                         {
                             #region RowHeader
                             ws.Cell("A2").Value = Msg.Translate(ctx, "AD_Org_ID");
@@ -156,72 +103,35 @@ namespace VIS.Models
                             ws.Cell("C2").Style.Font.Bold = true;
                             ws.Cell("D2").Value = Msg.Translate(ctx, "AMTACCTCR");
                             ws.Cell("D2").Style.Font.Bold = true;
-                            ws.Cell("E2").Value = Msg.Translate(ctx, "M_Product_ID");
-                            ws.Cell("E2").Style.Font.Bold = true;
-                            ws.Cell("F2").Value = Msg.Translate(ctx, "C_BPARTNER_ID");
-                            ws.Cell("F2").Style.Font.Bold = true;
-                            ws.Cell("G2").Value = Msg.GetMsg(ctx, "UserElement8_ID");
-                            ws.Cell("G2").Style.Font.Bold = true;
-                            ws.Cell("H2").Value = Msg.GetMsg(ctx, "UserElement1_ID");
-                            ws.Cell("H2").Style.Font.Bold = true;
-
-                            // Sender Detail
-                            ws.Cell("I2").Value = Msg.GetMsg(ctx, "UserElement4_ID");
-                            ws.Cell("I2").Style.Font.Bold = true;
-                            ws.Cell("J2").Value = Msg.GetMsg(ctx, "UserElement2_ID");
-                            ws.Cell("J2").Style.Font.Bold = true;
-                            ws.Cell("K2").Value = Msg.Translate(ctx, "C_Activity_ID");
-                            ws.Cell("K2").Style.Font.Bold = true;
-                            ws.Cell("L2").Value = Msg.Translate(ctx, "AD_Orgtrx_ID");
-                            ws.Cell("L2").Style.Font.Bold = true;
-                            ws.Cell("M2").Value = Msg.Translate(ctx, "C_Project_ID");
-                            ws.Cell("M2").Style.Font.Bold = true;
-
-                            // Receiver Detail
-                            ws.Cell("N2").Value = Msg.Translate(ctx, "C_Campaign_ID");
-                            ws.Cell("N2").Style.Font.Bold = true;
-                            ws.Cell("O2").Value = Msg.GetMsg(ctx, "UserElement9_ID");
-                            ws.Cell("O2").Style.Font.Bold = true;
-                            ws.Cell("P2").Value = Msg.GetMsg(ctx, "UserElement3_ID");
-                            ws.Cell("P2").Style.Font.Bold = true;
-                            ws.Cell("Q2").Value = Msg.GetMsg(ctx, "UserElement5_ID");
-                            ws.Cell("Q2").Style.Font.Bold = true;
-                            ws.Cell("R2").Value = Msg.GetMsg(ctx, "UserElement6_ID");
-                            ws.Cell("R2").Style.Font.Bold = true;
-                            ws.Cell("S2").Value = Msg.GetMsg(ctx, "UserElement7_ID");
-                            ws.Cell("S2").Style.Font.Bold = true;
+                            for (int i = 0; i < elementList.Count; i++)
+                            {
+                                ws.Cell(2, i + 5).Value = Msg.Translate(ctx, elementList[i]["ColumnName"]);
+                                ws.Cell(2, i + 5).Style.Font.Bold = true;
+                            }
                             #endregion
 
                             // Adding DataRows.
-                            for (int i = 0; i < dt.Rows.Count; i++)
+                            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
                             {
-                                // Parameter Detail
-                                ws.Cell("A" + (i + 3)).Value = dt.Rows[i]["AD_Org_ID"];
-                                ws.Cell("B" + (i + 3)).Value = dt.Rows[i]["account_id"];
-                                ws.Cell("C" + (i + 3)).Value = dt.Rows[i]["AMTACCTDR"];
-                                ws.Cell("D" + (i + 3)).Value = dt.Rows[i]["AMTACCTCR"];
-                                ws.Cell("E" + (i + 3)).Value = dt.Rows[i]["m_product_id"];
-                                ws.Cell("F" + (i + 3)).Value = dt.Rows[i]["C_BPARTNER_ID"];
-                                ws.Cell("G" + (i + 3)).Value = dt.Rows[i]["userelement8_id"];
-                                ws.Cell("H" + (i + 3)).Value = dt.Rows[i]["userelement1_id"];
-                                ws.Cell("I" + (i + 3)).Value = dt.Rows[i]["userelement4_id"];
-                                ws.Cell("J" + (i + 3)).Value = dt.Rows[i]["userelement2_id"];
-                                ws.Cell("K" + (i + 3)).Value = dt.Rows[i]["c_activity_id"];
-                                ws.Cell("L" + (i + 3)).Value = dt.Rows[i]["ad_orgtrx_id"];
-                                ws.Cell("M" + (i + 3)).Value = dt.Rows[i]["c_project_id"];
-                                ws.Cell("N" + (i + 3)).Value = dt.Rows[i]["c_campaign_id"];
-                                ws.Cell("O" + (i + 3)).Value = dt.Rows[i]["userelement9_id"];
-                                ws.Cell("P" + (i + 3)).Value = dt.Rows[i]["userelement3_id"];
-                                ws.Cell("Q" + (i + 3)).Value = dt.Rows[i]["userelement5_id"];
-                                ws.Cell("R" + (i + 3)).Value = dt.Rows[i]["userelement6_id"];
-                                ws.Cell("S" + (i + 3)).Value = dt.Rows[i]["userelement7_id"];
+                                ws.Cell(i + 3, 1).Value = ds.Tables[0].Rows[i]["AD_Org_ID"];
+                                ws.Cell(i + 3, 2).Value = ds.Tables[0].Rows[i]["ACCOUNT_ID"];
+                                ws.Cell(i + 3, 3).Value = ds.Tables[0].Rows[i]["ACCCREDIT"];
+                                ws.Cell(i + 3, 4).Value = ds.Tables[0].Rows[i]["ACCDEBIT"];
+                                for (int k = 0; k < elementList.Count; k++)
+                                {
+                                    if (elementList[k]["ElementType"].StartsWith("X"))
+                                    {
+                                        ws.Cell(i + 3, k + 5).Value = ds.Tables[0].Rows[i][elementList[k]["ElementType"]];
+                                    }
+                                    else
+                                    {
+                                        ws.Cell(i + 3, k + 5).Value = ds.Tables[0].Rows[i][elementList[k]["ColumnName"]];
+                                    }
+                                }
                             }
                         }
                         //Column width adjustment as per data
-                        ws.Column(1).AdjustToContents();
-                        ws.Column(2).AdjustToContents();
-                        ws.Column(3).AdjustToContents();
-                        ws.Column(4).AdjustToContents();
+                        ws.Columns().AdjustToContents();
                     }
                 }
                 using (MemoryStream stream = new MemoryStream())
@@ -294,39 +204,39 @@ namespace VIS.Models
                 columnName = Util.GetValueOfString(_elementlist["ColumnName"]);
                 if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X1"))
                 {
-                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement1_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement1_ID ");
+                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement1_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X1 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X2"))
                 {
-                    sql.Append(" ,  GetGLDimensionValue(NVL(fl.UserElement2_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement2_ID ");
+                    sql.Append(" ,  GetGLDimensionValue(NVL(fl.UserElement2_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X2 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X3"))
                 {
-                    sql.Append(" ,GetGLDimensionValue(NVL(fl.UserElement3_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement3_ID ");
+                    sql.Append(" ,GetGLDimensionValue(NVL(fl.UserElement3_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X3 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X4"))
                 {
-                    sql.Append(" ,GetGLDimensionValue(NVL(fl.UserElement4_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement4_ID ");
+                    sql.Append(" ,GetGLDimensionValue(NVL(fl.UserElement4_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X4 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X5"))
                 {
-                    sql.Append(" ,GetGLDimensionValue(NVL(fl.UserElement5_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement5_ID ");
+                    sql.Append(" ,GetGLDimensionValue(NVL(fl.UserElement5_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X5 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X6"))
                 {
-                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement6_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement6_ID ");
+                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement6_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X6 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X7"))
                 {
-                    sql.Append(" ,  GetGLDimensionValue(NVL(fl.UserElement7_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement7_ID ");
+                    sql.Append(" ,  GetGLDimensionValue(NVL(fl.UserElement7_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X7 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X8"))
                 {
-                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement8_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement8_ID ");
+                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement8_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X8 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("X9"))
                 {
-                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement9_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS UserElement9_ID ");
+                    sql.Append(" , GetGLDimensionValue(NVL(fl.UserElement9_ID,0),'" + columnName.Substring(0, columnName.Length - 3) + "') AS X9 ");
                 }
                 else if (Util.GetValueOfString(_elementlist["ElementType"]).Equals("OT"))
                 {
