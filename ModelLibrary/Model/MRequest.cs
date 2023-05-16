eb862e6b881276b786063d53f35be658588939ae
ajay.dwivedi <ajay.dwivedi@viennasolutions.com>
2023-05-16 14:30:18 +0800

    Bug Id 2105- Change in message based on role

--- ModelLibrary/Model/MRequest.cs
+++ ModelLibrary/Model/MRequest.cs
@@ -1026,35 +1026,38 @@ namespace VAdvantage.Model
             //VIS_427-Bug Id 2105: check when role changes for request
             if (CheckChange(ra, "AD_Role_ID"))
             {
+                string oldname="null", newname="null";
                 int AD_User_ID = p_ctx.GetAD_User_ID();
                 if (AD_User_ID == 0)
                 AD_User_ID = GetUpdatedBy();
-                Object oo = Get_ValueOld("AD_Role_ID");
-                int oldAd_Role_ID = 0;
-                string oldname = "";
-                if (oo is int)
+                int oldAd_Role_ID = Util.GetValueOfInt(Get_ValueOld("AD_Role_ID"));
+                String sql = "SELECT NAME,AD_Role_ID FROM AD_Role where AD_Role_ID IN " + (oldAd_Role_ID, GetAD_Role_ID());
+               DataSet ds = DB.ExecuteDataset(sql);
+                if( ds !=null && ds.Tables[0].Rows.Count>0)
                 {
-                    oldAd_Role_ID = ((int)oo);
-                    MRole role = new MRole(p_ctx, oldAd_Role_ID, null);
-                     oldname = role.GetName();
-                }
-                if (oldAd_Role_ID != 0)
-                {
-                    MRole role = new MRole(p_ctx, GetAD_Role_ID(), null);
-                    string newname = role.GetName();
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        if (oldAd_Role_ID == Util.GetValueOfInt(dr["AD_Role_ID"]) && oldAd_Role_ID != 0)
+                        {
+                            oldname = Util.GetValueOfString(dr["Name"]);
+                        }
+                        if (GetAD_Role_ID() == Util.GetValueOfInt(dr["AD_Role_ID"]) && GetAD_Role_ID() != 0)
+                        {
+                            newname = Util.GetValueOfString(dr["Name"]);
+                        }
+                    }
                     //  RequestActionTransfer - Request {0} was transfered by {1} from {2} to {3}
                     Object[] args = new Object[] {GetDocumentNo(),
                         MUser.GetNameOfUser(AD_User_ID),
-                        oldname,
-                         newname
+                         oldname,
+                          newname
                         };
                     String msg = Msg.GetMsg(GetCtx(), "RequestActionTransfer");
                     //VIS_427-Bug Id 2105: replacing  the value of placeholders in above message
-                    msg = String.Format(@msg, Util.GetValueOfString(args[0].ToString()), Util.GetValueOfString(args[1].ToString()), Util.GetValueOfString(args[2].ToString()), Util.GetValueOfString(args[3].ToString()));
+                    msg = String.Format(@msg, args[0], args[1], args[2], args[3]);
                     AddToResult(msg);
                     sendInfo.Add("AD_Role_ID");
                 }
-
             }
             //
             if (CheckChange(ra, "Priority"))
