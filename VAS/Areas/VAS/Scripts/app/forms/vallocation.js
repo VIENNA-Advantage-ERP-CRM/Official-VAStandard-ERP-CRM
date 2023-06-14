2c8471f7b2247c098ebc6f83e67fe31511d253aa
ajay.dwivedi <ajay.dwivedi@viennasolutions.com>
2023-06-14 16:16:39 +0800

    DevOps TaskID:2173 Filter Business Partner According to organization

--- VAS/Areas/VAS/Scripts/app/forms/vallocation.js
+++ VAS/Areas/VAS/Scripts/app/forms/vallocation.js
@@ -2134,8 +2134,9 @@
                     $cmbCurrency.addClass('vis-ev-col-mandatory');
                 }
             }
-
-            value = VIS.MLookupFactory.getMLookUp(ctx, self.windowNo, 3499, VIS.DisplayType.Search);
+            //VIS_427 DevOps TaskID:2173 Filter Business Partner According to organization
+            value = VIS.MLookupFactory.get(ctx, self.windowNo, 3499, VIS.DisplayType.Search, "C_BPartner_ID", 0, false,
+                "C_BPartner.IsActive = 'Y' AND C_BPartner.AD_Org_ID IN (0, @OrgID@)");
             $vSearchBPartner = new VIS.Controls.VTextBoxButton("C_BPartner_ID", true, false, true, VIS.DisplayType.Search, value);
 
             //VIS_0045: create and assin control to conversion type
