bb10c646214f98c8239c51e168db90c71cd5f692
ajay.dwivedi <ajay.dwivedi@viennasolutions.com>
2023-07-01 06:14:17 -0600

    BugId: 2190 Corrected the time lapse on date last action field

--- ModelLibrary/Model/MRequest.cs
+++ ModelLibrary/Model/MRequest.cs
@@ -1136,11 +1136,14 @@ namespace VAdvantage.Model
                 _emailTo = new StringBuilder();
                 if (_requestType.IsR_AllowSaveNotify() && (update != null || sendInfo.Count > 0))
                 {
-                    DateTime date= DateTime.SpecifyKind(ra.GetCreated(), DateTimeKind.Utc);  //VIS_427 BugId: 2190 Corrected the time lapse on date last action field
                     prepareNotificMsg(sendInfo);
                     //	Update
                     if (ra != null)
+                    {
+                        //VIS_427 BugId: 2190 Corrected the time lapse on date last action field
+                        DateTime date = ra.GetCreated().ToLocalTime();
                         SetDateLastAction(date);
+                    }
                     SetLastResult(GetResult());
                     SetDueType();
                     //	ReSet
