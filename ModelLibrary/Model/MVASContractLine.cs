8da9c18a8d8f89f555458c0d47671e4988dedc73
Sukhvinder Singh <sukhvinder.singh@viennaadvantage.com>
2023-11-29 10:43:31 +0530

    1.Freeze tabs in ContractMaster on Publish & Unfreeze on Reactivate . 2.Renewal Date should not be greater than current date. & Amount Should be greater than Utilize amount of contract. 3. In sales order set Line Total Amount  zero on Void.

--- ModelLibrary/Model/MVASContractLine.cs
+++ ModelLibrary/Model/MVASContractLine.cs
@@ -33,6 +33,41 @@ namespace VAdvantage.Model
         /// <returns>true if can be saved</returns>
         protected override Boolean BeforeSave(Boolean newRecord)
         {
+            //VAI050-To check Contract exists in SO,PO,BPO,BSO,AR Invoice,AP Invoice
+            if (!newRecord && (Is_ValueChanged("M_Product_ID") || Is_ValueChanged("M_AttributeSetInstance_ID")
+                || Is_ValueChanged("C_Charge_ID") || Is_ValueChanged("Amount")))
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT  VAS_ContractMaster_ID,(SELECT VAS_ContractUtilizedAmount FROM VAS_contractmaster WHERE " +
+                            "VAS_Contractmaster_Id = " + GetVAS_ContractMaster_ID() + ") UtilizeAmount  FROM C_Order WHERE VAS_ContractMaster_ID = " + GetVAS_ContractMaster_ID() + " UNION " +
+                        " SELECT VAS_ContractMaster_ID,(SELECT VAS_ContractUtilizedAmount FROM VAS_contractmaster WHERE " +
+                            "VAS_Contractmaster_Id = " + GetVAS_ContractMaster_ID() + ") UtilizeAmount    FROM C_Invoice WHERE VAS_ContractMaster_ID = " + GetVAS_ContractMaster_ID());
+                DataSet ds = DB.ExecuteDataset(sql.ToString(), null, Get_Trx());
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (Is_ValueChanged("M_Product_ID") || Is_ValueChanged("M_AttributeSetInstance_ID")
+                     || Is_ValueChanged("C_Charge_ID"))
+                    {
+                        log.SaveError("", Msg.GetMsg(GetCtx(), "VAS_CheckOrder"));
+                        return false;
+                    }                  
+                     else 
+                    {
+                        int UtilizeAmount = Util.GetValueOfInt(ds.Tables[0].Rows[0]["UtilizeAmount"]);
+                        sql.Clear();
+                        sql.Append("SELECT NVL(SUM(Amount),0) TotalAmount FROM  VAS_ContractLine WHERE VAS_ContractMaster_Id=" 
+                            + GetVAS_ContractMaster_ID() + " AND VAS_ContractLine_Id NOT IN(" + GetVAS_ContractLine_ID() + ")");
+                        int TotalAmount = Util.GetValueOfInt(DB.ExecuteScalar(sql.ToString(), null, Get_Trx()));
+                        TotalAmount = TotalAmount + Util.GetValueOfInt(GetAmount()); //All Contract Lines Amount Total
+                        if (UtilizeAmount > TotalAmount) // Total Amount should be greater than utilize amount
+                        {
+                            log.SaveError("", Msg.GetMsg(GetCtx(), "VAS_UtilizeAmount"));
+                            return false;
+                        }
+                    }
+                }
+            }
+
             //	Product/Charge Must be selected
             if (newRecord || Is_ValueChanged("M_Product_ID") || Is_ValueChanged("C_Charge_ID"))
             {
