ebac52eca273e8ee2321dcb9ca38ed77b4c32ba7
ajay.dwivedi <ajay.dwivedi@viennasolutions.com>
2024-01-18 04:57:24 -0600

    Devops ID: 4261 Created tab panel on Invoice tab to get Invoice tax data

---
+++ VAS/Areas/VAS/Scripts/app/forms/InvoiceTaxTabPanel.js
@@ -0,0 +1,123 @@
+﻿/************************************************************
+ * Module Name    : VAS
+ * Purpose        : Tab Panel created to get Invoice Tax Data
+ * chronological  : Development
+ * Created Date   : 16 January 2024
+ * Created by     : VIS_427
+ ***********************************************************/
+; VAS = window.VAS || {};
+; (function (VAS, $) {
+
+
+    VAS.InvoiceTaxTabPanel = function () {
+
+        this.record_ID = 0;
+        this.curTab = null;
+        this.selectedRow = null;
+        this.panelWidth;
+        var lblTitle = null;
+        var ctx = this.ctx;
+
+        var elements = [
+            "VAS_InvoiceTaxTitleTabPanel",
+            "VAS_InvoiceTax",
+            "VAS_InvoiceTaxPaybaleAmt",
+            "VAS_InvoiceTaxAmt",
+            "VAS_IsIncludeTax",
+        ];
+
+        VAS.translatedTexts = VIS.Msg.translate(ctx, elements, true);
+
+        var $root = $('<div class = "root"></div>');
+        var wrapperDiv = $("<div class ='vas-apTaxWrapper'></div>")
+        lblTitle = $("<h6 class='vas-apTax-ttl'>" + VAS.translatedTexts.VAS_InvoiceTaxTitleTabPanel + "</h6>");
+        this.init = function () {
+            wrapperDiv.append(lblTitle);
+            InvoiceTaxPanel();
+            $root.append(wrapperDiv);
+        };
+        //Function defined to append design
+        function InvoiceTaxPanel() {
+            wrapperDiv.append(
+                '<div class="vas-aptaxListGroup">' +
+                '<div id="VAS-TaxDetail_' + this.windowNo + '" class= "VAS-TaxDetail mb-2" > ' +
+                '</div>' +
+                '</div>');
+        };
+        /*This function is used to get invoice tax data*/
+        this.getInvoiceTaxData = function (recordID) {
+            $.ajax({
+                url: VIS.Application.contextUrl + "VAS/PoReceipt/GetTaxData",
+                type: "GET",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                data: { InvoiceId: recordID },
+                success: function (data) {
+                    if (JSON.parse(data) != "") {
+                        data = JSON.parse(data);
+                        console.log(data);
+                        if (data != null && data.length > 0) {
+                            for (i = 0; i < data.length; i++) {
+                                var TabPaneldesign = '<div class="vas-apListItem mb-2">' +
+                                    '<div class="vas-ap-sglItem mb-2">' +
+                                    '<div class="vas-singleTaxElement vas-setTaxWidth">' +
+                                    '<span class="vas-singleTaxElementTTl font-weight-bold">' + VAS.translatedTexts.VAS_InvoiceTax + '</span>' +
+                                    '<span class="vas-singleTaxElementValue">' + data[i].TaxName + '</span>' +
+                                    '</div>' +
+                                    '<div class="vas-singleTaxElement vas-setTaxPaybleAmtWidth">' +
+                                    '<span class="vas-singleTaxElementTTl font-weight-bold">' + VAS.translatedTexts.VAS_InvoiceTaxPaybaleAmt + '</span>' +
+                                    '<span class="vas-singleTaxElementValue">' + (data[i].TaxPaybleAmt).toLocaleString(window.navigator.language, { minimumFractionDigits: data[i].stdPrecision, maximumFractionDigits: data[i].stdPrecision }) + '</span>' +
+                                    '</div>' +
+                                    '<div class="vas-singleTaxElement vas-setTaxAmtWidth">' +
+                                    '<span class="vas-singleTaxElementTTl font-weight-bold">' + VAS.translatedTexts.VAS_InvoiceTaxAmt + '</span>' +
+                                    '<span class="vas-singleTaxElementValue">' + (data[i].TaxAmt).toLocaleString(window.navigator.language, { minimumFractionDigits: data[i].stdPrecision, maximumFractionDigits: data[i].stdPrecision }) + '</span>' +
+                                    '</div>' +
+                                    '</div >' +
+                                    '<div class="vas-apItem-checkbox d-flex align-items-center">' +
+                                    '<input type="checkbox" id="vas-includeTaxCheckbox" ' + (data[i].IsTaxIncluded == "Y" ? 'checked' : '') + '>' +
+                                    '<label class="vas-apcheckbox-label ml-1" for="vas-includeTaxCheckbox">' + VAS.translatedTexts.VAS_IsIncludeTax + '</label>' +
+                                    '</div>' +
+                                    '</div>'
+                                 //Appending design to wrapperDiv
+                                 wrapperDiv.find('#VAS-TaxDetail_' + this.windowNo).append(TabPaneldesign);
+                            }
+
+                        }
+                    }
+                },
+                error: function (eror) {
+                    console.log(eror);
+                }
+            })
+        }
+
+        this.getRoot = function () {
+            return $root;
+        };
+
+        VAS.InvoiceTaxTabPanel.prototype.startPanel = function (windowNo, curTab) {
+            this.windowNo = windowNo;
+            this.curTab = curTab;
+            this.init();
+        };
+        /* This function to update tab panel based on selected record */
+        VAS.InvoiceTaxTabPanel.prototype.refreshPanelData = function (recordID, selectedRow) {
+            this.record_ID = recordID;
+            this.selectedRow = selectedRow;
+            this.getInvoiceTaxData(recordID);
+        };
+        /* This will set width as per window width in dafault case it is 75% */
+        VAS.InvoiceTaxTabPanel.prototype.sizeChanged = function (width) {
+            this.panelWidth = width;
+        };
+        /* Disposing all variables from memory */
+        VAS.InvoiceTaxTabPanel.prototype.dispose = function () {
+            this.record_ID = 0;
+            this.windowNo = 0;
+            this.curTab = null;
+            this.rowSource = null;
+            this.panelWidth = null;
+            lblTitle = null;
+        }
+    }
+})(VAS, jQuery);
--- VASLogic/Models/PoReceiptTabPanelModel.cs
+++ VASLogic/Models/PoReceiptTabPanelModel.cs
@@ -68,6 +68,38 @@ namespace VASLogic.Models
             }
             return tabPanels;
         }
+
+        /// <summary>
+        /// 16/1/2024 This function is Used to Get the Invoice tax data 
+        /// </summary>
+        /// <param name="ctx">Context</param>
+        /// <param name="InvoiceId">Invoice ID</param>
+        /// <Author>VIS_427 Devops ID: 4261</Author>
+        /// <returns>returns the Invoice tax data</returns>
+        public List<TaxTabPanel> GetInvoiceTaxData(Ctx ctx, int InvoiceId)
+        {
+            List<TaxTabPanel> InvocieTaxTabPanel = new List<TaxTabPanel>();
+            String sql = @"SELECT t.Name,ct.TaxAmt,ct.TaxBaseAmt,ct.IsTaxIncluded,cy.StdPrecision FROM C_InvoiceTax ct 
+                          INNER JOIN C_Invoice ci ON (ci.C_Invoice_ID = ct.C_Invoice_ID) 
+                          INNER JOIN C_Tax t ON (t.C_Tax_ID = ct.C_Tax_ID) 
+                          INNER JOIN C_Currency cy ON (cy.C_Currency_ID = ci.C_Currency_ID) WHERE ct.C_Invoice_ID = " + InvoiceId + " Order By t.Name";
+
+            DataSet ds = DB.ExecuteDataset(sql, null, null);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    TaxTabPanel obj = new TaxTabPanel();
+                    obj.TaxName = Util.GetValueOfString(ds.Tables[0].Rows[i]["Name"]);
+                    obj.TaxPaybleAmt = Util.GetValueOfDecimal(ds.Tables[0].Rows[i]["TaxBaseAmt"]);
+                    obj.TaxAmt = Util.GetValueOfDecimal(ds.Tables[0].Rows[i]["TaxAmt"]);
+                    obj.IsTaxIncluded = Util.GetValueOfString(ds.Tables[0].Rows[i]["IsTaxIncluded"]);
+                    obj.stdPrecision = Util.GetValueOfInt(ds.Tables[0].Rows[i]["StdPrecision"]);
+                    InvocieTaxTabPanel.Add(obj);
+                }
+            }
+            return InvocieTaxTabPanel;
+        }
     }
     public class TabPanel
     {
@@ -82,6 +114,19 @@ namespace VASLogic.Models
         public string ProductName { get; set; }
 
         public string AttributeSetInstance { get; set; }
+
+    }
+
+    public class TaxTabPanel
+    {
+        public string TaxName { get; set; }
+
+        public decimal TaxPaybleAmt { get; set; }
+
+        public decimal TaxAmt { get; set; }
+
+        public string IsTaxIncluded { get; set; }
+        public int stdPrecision { get; set; }
 
     }
 }
