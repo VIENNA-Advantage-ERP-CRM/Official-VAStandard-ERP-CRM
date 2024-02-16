ba7365e40235124eb62aa90103ed3b0185dc840e
Priyanka Sharma <priyanka.sharma@viennaadvantage.com>
2024-02-16 13:29:08 +0530

    changes done for update requition qty when product change .

--- VAS/Areas/VAS/Scripts/model/calloutrequisition.js
+++ VAS/Areas/VAS/Scripts/model/calloutrequisition.js
@@ -46,9 +46,9 @@
             // JID_0910: On change of product on line system is not removing the ASI. if product is changed then also update the ASI field.
             mTab.setValue("M_AttributeSetInstance_ID", null);
             var C_BPartner_ID = ctx.getContextAsInt(windowNo, "C_BPartner_ID");
-            var qty = mTab.getValue("Qty");
-           //VAI051:- When change the Product then  RequitionQty and Qauntity should be updated 
+            //VAI051:- When change the Product then  RequitionQty and Qauntity should be updated 
             mTab.setValue("Qty", mTab.getValue("QtyEntered"));
+            var qty = mTab.getValue("Qty");
             var isSOTrx = false;
             var M_PriceList_ID = ctx.getContextAsInt(windowNo, "M_PriceList_ID");
             var M_PriceList_Version_ID = ctx.getContextAsInt(windowNo, "M_PriceList_Version_ID");
