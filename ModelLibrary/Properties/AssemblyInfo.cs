cb938bf90e14c89343f746fb7351dcf92fc3fa26
Pre Live <preliveserver@viennasolutions.com>
2024-03-26 07:57:09 +0000

    1.4.0.6

--- ModelLibrary/Properties/AssemblyInfo.cs
+++ ModelLibrary/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.0")]
 
-[assembly: AssemblyFileVersion("2.4.0.4")]
+[assembly: AssemblyFileVersion("2.4.0.5")]
 
