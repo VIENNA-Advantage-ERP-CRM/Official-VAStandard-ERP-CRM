4c1eceb7a4c732d1985749fb3e734688661d2645
Sukhvinder Singh <sukhvinder.singh@viennaadvantage.com>
2024-09-11 12:07:59 +0530

    1.Changes in lookup variable

--- VAS/Areas/VAS/Scripts/app/forms/VAS_LowestPrMonthlySalesWidget.js
+++ VAS/Areas/VAS/Scripts/app/forms/VAS_LowestPrMonthlySalesWidget.js
@@ -23,6 +23,7 @@
         var OrganizationUnit = 0;
         var stdPrecision = 0;
         var symbol = "";
+        var $vOrg;
 
         function createStructure(widgetID) {
             var $contentContainer = $('<div class="VAS-org-lowest-unitsales-col VAS-content-container vis-formouterwrpdiv">' +
@@ -203,7 +204,7 @@
             $root.find('.repfix-resultTxt').text(percentageDifference);
 
             // Adjust the result icon based on the percentage difference
-            var resultIcon = $root.find('.vis');
+            var resultIcon = $root.find('.VAS-graph-result .vis');
             if (percentageDifference.startsWith('-')) {
                 resultIcon.removeClass('vis-trending-up').addClass('vis-trending-down');
             } else {
@@ -356,10 +357,6 @@
 
         /* This function is used to refresh the widget data */
         this.refreshWidget = function () {
-            // Reset the dropdown list value
-            if ($vOrg) {
-                $vOrg.setValue(0);
-            }
             chartInstance = null;
             $self.currentPage = 1;
             $self.totalPages = 0;
