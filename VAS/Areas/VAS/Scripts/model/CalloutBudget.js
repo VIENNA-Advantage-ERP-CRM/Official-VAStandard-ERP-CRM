be45bf91f7994fac8605bd6eac74851578209fba
Ajay Dwivedi <ajay.dwivedi@viennasolutions.com>
2024-10-25 13:37:37 +0530

    created to set the value of year and period null when Budget Control Basis Field is changed

---
+++ VAS/Areas/VAS/Scripts/model/CalloutBudget.js
@@ -0,0 +1,42 @@
+﻿/********************************************************************************
+ * Module Name    : VAS
+ * Purpose        : This Callout is created to set the value of year and period 
+                    null when Budget Control Basis Field is changed
+ * chronological  : Development
+ * Created Date   : 25 Oct 2024
+ * Created by     : VIS_427
+ ********************************************************************************/
+
+; (function (VIS, $) {
+
+    function CalloutBudget() {
+        VIS.CalloutEngine.call(this, "VIS.CalloutBudget"); //must call
+    }
+    VIS.Utility.inheritPrototype(CalloutBudget, VIS.CalloutEngine);//inherit CalloutEngine
+    /// <summary>
+    ///Setting the value of year and period null when Budget Control Basis Field is changed
+    /// </summary>
+    /// <param name="ctx">Context</param>
+    /// <param name="WindowNo">current Window No</param>
+    /// <param name="mTab">Model Tab</param>
+    /// <param name="mField">Model Field</param>
+    /// <param name="value">The new value</param>
+    /// <returns>""</returns>
+    CalloutBudget.prototype.SetYearAndPeriodNull = function (ctx, windowNo, mTab, mField, value, oldValue) {
+        if (value == null || value.toString() == "") {
+            return "";
+        }
+        if (mTab.getValue("BudgetControlBasis") == "A")
+        {
+            mTab.setValue("C_Period_ID", null)
+        }
+        else if (mTab.getValue("BudgetControlBasis") == "P")
+        {
+            mTab.setValue("C_Year_ID", null)
+        }
+        ctx = windowNo = mTab = mField = value = oldValue = null;
+        return "";
+    };
+    VIS.Model.CalloutBudget = CalloutBudget;
+
+})(VIS, jQuery);
