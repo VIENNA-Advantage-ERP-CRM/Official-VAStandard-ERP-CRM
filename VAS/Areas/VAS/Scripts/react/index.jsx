907f694c2fec2f1e0b2173486ae8ed8f93e39a54
Bharat Singla <bharat.singla@onfinity.io>
2026-05-06 13:35:28 +0530

    changes in index file

--- VAS/Areas/VAS/Scripts/react/index.jsx
+++ VAS/Areas/VAS/Scripts/react/index.jsx
@@ -1,21 +1,19 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//VIS Replace with module prefix
+// Ensure VAS namespace and dependencies are available
+window.VAS = window.VAS || {};
+(function (VAS, $) {
+    // Ensure VAS.React namespace is initialized
+    VAS.React = VAS.React || {};
 
-// Ensure VIS namespace and dependencies are available
-window.VIS = window.VIS || {};
-(function (VIS, $) {
-    // Ensure VIS.React namespace is initialized
-    VIS.React = VIS.React || {};
-
-    // Constructor function for VIS.React
-    VIS.React = function () {
+    // Constructor function for VAS.React
+    VAS.React = function () {
         this.listener=null;
     };
 
     // Initialize method to render React component dynamically
-    VIS.React.prototype.init = function (windowNo, frame, additionalInfo) {
+    VAS.React.prototype.init = function (windowNo, frame, additionalInfo) {
         this.windowNo = windowNo;
         this.frame = frame;
         var componentName = frame.componentName;
@@ -32,15 +30,15 @@ window.VIS = window.VIS || {};
         );
     };
 
-    VIS.React.prototype.widgetFirevalueChanged = function (value) {
+    VAS.React.prototype.widgetFirevalueChanged = function (value) {
         // this.getRoot().trigger('widgetFirevalueChanged', value); // Trigger custom event with the value
         if (this.listener)
             this.listener.widgetFirevalueChanged(value);
     };
 
-    VIS.React.prototype.addChangeListener = function (listener) {
+    VAS.React.prototype.addChangeListener = function (listener) {
         this.listener = listener;
     }
 
-})(VIS, jQuery);
+})(VAS, jQuery);
 
