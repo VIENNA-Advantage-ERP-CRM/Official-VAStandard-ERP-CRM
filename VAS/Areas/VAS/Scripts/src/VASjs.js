b7e89490c21277dcefb8449a4a16dcc4932997ec
liveserver <live@viennasolutions.com>
2026-05-25 07:15:19 +0000

    Project chkin

--- VAS/Areas/VAS/Scripts/src/VASjs.js
+++ VAS/Areas/VAS/Scripts/src/VASjs.js
@@ -122,3 +122,6 @@ import "../app/widgets/OutstandingSalesOrderWidget.js";
 import "../app/widgets/OverdueWidget.js";
 import "../app/widgets/PaidthismonthWidget.js";
 import "../app/widgets/TopDebtorsWidget.js";
+import "../app/widgets/SalesInvoiceCustomerSearchWidget.js";
+import "../app/tabpanel/VAS_InvoiceOverview.js";
+import "../app/tabpanel/VAS_PaymentVoucherPanel.js";
