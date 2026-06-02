aa7f8ecc1bd83c8112fa4ce9b569ba970239cb10
Amit Bansal <amit.bansal@onfinity.io>
2026-06-02 09:31:31 +0530

    UnReconciled Receipt Widget

--- VAS/Areas/VAS/Scripts/src/VASjs.js
+++ VAS/Areas/VAS/Scripts/src/VASjs.js
@@ -123,6 +123,7 @@ import "../app/widgets/BouncedChequesWidget.js";
 import "../app/widgets/CustomerPaidMethodWidget.js";
 import "../app/widgets/VAS_001_CollectionEfficiencyWidget.js";
 import "../app/widgets/VAS_003_OnAccountReceipts.js";
+import "../app/widgets/VAS_011_UnreconciledReceipts.js";
 import "../app/widgets/ExpectedReceiptsWidget.js";
 import "../app/widgets/RequestWidget.js";
 import "../app/widgets/AgingReceivablesWidget.js";
