8b3238e2f3e44825a0daf0b8878a2b6e2fbc9131
neha.thakur <neha.thakur@VAI-MOH-LAP-01>
2026-08-18 18:03:31 +0530

    Sales Quotation bottom panel

--- VAS/Areas/VAS/Scripts/src/VASjs.js
+++ VAS/Areas/VAS/Scripts/src/VASjs.js
@@ -182,6 +182,7 @@ import "../app/util/CurrencyFormat.js";
 import "../app/util/AttributeControl.js";
 import "../app/util/VAS_PanelUtil.js";
 import "../app/util/VAS_ZoomUtil.js";
+import "../app/util/VAS_PanelShortcuts.js";
 import "../app/widgets/RequestWidget.js";
 import "../app/widgets/AgingReceivablesWidget.js";
 import "../app/widgets/AvgDaysToPayWidget.js";
